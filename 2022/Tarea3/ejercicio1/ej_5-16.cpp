563868fedc77eed0b4a0735745717911660dfd7b
DSarceno <dsarceno68@gmail.com>
2022-10-14 17:27:03 -0600

    t3

---
+++ 2022/Tarea3/ejercicio1/ej_5-16.cpp
@@ -0,0 +1,18 @@
+// vie 14 oct 2022 17:25:37 CST 
+// ej_5-16.cpp 
+// Diego Sarceno (dsarceno68@gmail.com) 
+
+// Resumen 
+
+// Codificado del texto: UTF8 
+// Compiladores probados: g++ (Ubuntu 20.04 Linux) 9.4.0 
+// Instruciones de Ejecución: no requiere nada mas 
+// g++ -Wall -c -o ej_5-16.o ej_5-16.cpp 
+// g++ -o ej_5-16.x ej_5-16.o 
+
+
+// Librerias 
+#include <iostream> 
+
+using namespace std; 
+
---
+++ 2022/Tarea3/ejercicio3/ej_5-18.cpp
@@ -0,0 +1,18 @@
+// vie 14 oct 2022 17:26:22 CST 
+// ej_5-18.cpp 
+// Diego Sarceno (dsarceno68@gmail.com) 
+
+// Resumen 
+
+// Codificado del texto: UTF8 
+// Compiladores probados: g++ (Ubuntu 20.04 Linux) 9.4.0 
+// Instruciones de Ejecución: no requiere nada mas 
+// g++ -Wall -c -o ej_5-18.o ej_5-18.cpp 
+// g++ -o ej_5-18.x ej_5-18.o 
+
+
+// Librerias 
+#include <iostream> 
+
+using namespace std; 
+
